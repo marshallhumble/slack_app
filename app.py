bdd31895805ceb2767855fb31b7a1e591b4875cd
marshallhumble <humblejm@gmail.com>
2015-12-04 22:00:00 -0600

    added in flask app and index.html , just geetting the forms right then will integrate func  to page.

--- app.py
+++ app.py
@@ -1,40 +1,49 @@
-from flask import Flask, render_template
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required, Length
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top secret!'
 bootstrap = Bootstrap(app)
 
 
+def delete_my_files(token, domain, user, time):
+    deleted_list = []
+    while 1:
+        files_list_url = 'https://slack.com/api/files.list'
+        date = str(calendar.timegm((datetime.now() + timedelta(-time)).utctimetuple()))
+        data = {"token": token, "ts_to": date, "user": user}
+        response = requests.post(files_list_url, data=data)
+        if len(response.json()["files"]) == 0:
+            break
+        for f in response.json()["files"]:
+            deleted_list.append( f["name"])
+            timestamp = str(calendar.timegm(datetime.now().utctimetuple()))
+            delete_url = "https://" + domain + ".slack.com/api/files.delete?t=" + timestamp
+            requests.post(delete_url, data={
+                "token": token,
+                "file": f["id"],
+                "set_active": "true",
+                "_attempts": "1"})
+    return deleted_list
+
+"""
 class NameForm(Form):
     user = StringField('What is your username?', validators=[Required(), Length(1, 32)])
     domain = StringField('What is your team name?', validators=[Required(), Length(1, 16)])
     token = StringField('What is you API token?', validators=[Required(), Length(1, 40)])
     time = StringField('Enter in the # of days to limit (not required)', validators=[Length(1, 3)])
     submit = SubmitField('Submit')
-
+"""
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    token = None
-    domain = None
-    user = None
-    time = None
+    token = request.values.get('token')
+    domain = request.values.get('domain')
+    user = request.values.get('user')
+    time = request.values.get('time')
 
-    form = NameForm()
-    if form.validate_on_submit():
-        token = form.token.data
-        form.token.data = ''
-        domain = form.domain.data
-        form.domain.data = ''
-        user = form.user.data
-        form.user.data = ''
-        time = form.time.data
-        form.time.data = ''
-    return render_template('index.html', form=form, token=token, domain=domain, user=user, time=time)
+    if slack_app:
+        return render_template('index.html', form=form, token=token, domain=domain, user=user, time=time)
 
 
 if __name__ == '__main__':
