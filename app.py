465254a51e5cf115ae99584f1b95acc736ae02f8
marshallhumble <humblejm@gmail.com>
2015-12-04 19:49:35 -0600

    added in flask app and index.html , just geetting the forms right then will integrate func  to page.

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import Required, Length
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top secret!'
+bootstrap = Bootstrap(app)
+
+
+class NameForm(Form):
+    user = StringField('What is your username?', validators=[Required(), Length(1, 32)])
+    domain = StringField('What is your team name?', validators=[Required(), Length(1, 16)])
+    token = StringField('What is you API token?', validators=[Required(), Length(1, 40)])
+    time = IntegerField('Enter in the # of days to limit (not required)', widget=NumberInput())
+
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    token = None
+    domain = None
+    user = None
+    time = None
+
+    form = NameForm()
+    if form.validate_on_submit():
+        token = form.token.data
+        form.token.data = ''
+        domain = form.domain.data
+        form.domain.data = ''
+        user = form.user.data
+        form.user.data = ''
+        time = form.time.data
+        form.time.data = ''
+    return render_template('index.html', form=form, token=token, domain=domain, user=user, time=time)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
--- slack.py
+++ slack.py
@@ -3,23 +3,23 @@
 import requests
 import calendar
 from datetime import datetime, timedelta
-from settings import _token, _domain, _user, _time, _pretty
+from settings import token, domain, user, time, pretty
 
 
 def delete_my_files():
     while 1:
         files_list_url = 'https://slack.com/api/files.list'
-        date = str(calendar.timegm((datetime.now() + timedelta(-_time)).utctimetuple()))
-        data = {"token": _token, "ts_to": date, "user": _user}
+        date = str(calendar.timegm((datetime.now() + timedelta(-time)).utctimetuple()))
+        data = {"token": token, "ts_to": date, "user": user}
         response = requests.post(files_list_url, data=data)
         if len(response.json()["files"]) == 0:
             break
         for f in response.json()["files"]:
             print("Deleting file " + f["name"] + "...")
             timestamp = str(calendar.timegm(datetime.now().utctimetuple()))
-            delete_url = "https://" + _domain + ".slack.com/api/files.delete?t=" + timestamp
+            delete_url = "https://" + domain + ".slack.com/api/files.delete?t=" + timestamp
             requests.post(delete_url, data={
-                "token": _token,
+                "token": token,
                 "file": f["id"],
                 "set_active": "true",
                 "_attempts": "1"})
